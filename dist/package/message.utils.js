57d2c4b135efa9dc9eac88cf477461ea98eb2ddb
gabrielcruzsoto <gcruz@movix.com>
2023-01-06 14:41:52 -0300

    correcion a la libreria layer

---
+++ dist/package/message.utils.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.messageLogs = void 0;
+const messageLogs = () => {
+    console.log('message');
+};
+exports.messageLogs = messageLogs;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWVzc2FnZS51dGlscy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy91dGlscy9tZXNzYWdlLnV0aWxzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFPLE1BQU0sV0FBVyxHQUFHLEdBQUUsRUFBRTtJQUMzQixPQUFPLENBQUMsR0FBRyxDQUFDLFNBQVMsQ0FBQyxDQUFBO0FBQzFCLENBQUMsQ0FBQTtBQUZZLFFBQUEsV0FBVyxlQUV2QiIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBjb25zdCBtZXNzYWdlTG9ncyA9ICgpPT57XG4gICAgY29uc29sZS5sb2coJ21lc3NhZ2UnKVxufSJdfQ==
---
+++ lib/main.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib'
+import { CdkLayerStack } from './stacks/cdkLayerStack'
+
+
+const devEnv = {
+    account: '698852667105',
+    region: 'us-east-1',
+}
+
+const app = new cdk.App()
+new CdkLayerStack( app, 'CdkLayerStack', { env: devEnv } )
